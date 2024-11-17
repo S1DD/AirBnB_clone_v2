8af53688b4622d4eb7fb41a6781a75eaf7b0a5de
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-11-17 15:37:19 -0800

    Removed whitespaces

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,31 +16,31 @@ class FileStorage:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
     """
-    
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """ Returns the dictionary __objects """
         return FileStorage.__objects
-    
+
     def new(self, obj):
         """ Adds a new object to __objects with key <obj class name>.id """
 
         objcname = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(objcname, obj.id)] = obj
-    
+
     def save(self):
         """ Serializes __objects to the JSON file. """
-        
+
         odict = FileStorage.__file_path
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
         with open(FileStorage.__file_path, "w") as f:
-                json.dump(objdict, f)
-    
+            json.dump(objdict, f)
+
     def reload(self):
         """Deserializes the JSON file to __objects, if the file exists."""
-        
+
         try:
             with open(FileStorage.__file_path) as f:
                 json_objs = json.load(f)
@@ -48,6 +48,6 @@ class FileStorage:
                     cls_name = obj["__class__"]
                     del obj["__class__"]
                     self.new(eval(cls_name)(**o))
-        
+
         except FileNotFoundError:
             return
