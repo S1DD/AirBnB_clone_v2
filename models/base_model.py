63f99d3b1f2da851ff618a92aaa8a3ab7b7a7323
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-11-19 02:57:50 -0800

    Added a condition to assign the id to the object if it isn't specified

--- models/base_model.py
+++ models/base_model.py
@@ -8,23 +8,39 @@ import models
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
 
+    """Base class for all our classes"""
+
     def __init__(self, *args, **kwargs):
+        """ deserialize and serialize a class """
         """Initialize a new BaseModel.
         Args:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
         """
+
         tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
+
+        if 'id' not in kwargs:
+            self.id = str(uuid.uuid4())
+        else:
+            self.id = kwargs['id']
+
+        if 'created_at' not in kwargs:
+            self.created_at = datetime.today()
+        else:
+            self.created_at = datetime.strptime(kwargs['created_at'], tform)
+
+        if 'updated_at' not in kwargs:
+            self.updated_at = datetime.today()
+        else:
+            self.updated_at = datetime.strptime(kwargs['updated_at'], tform)
+
+        if kwargs:
             for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
+                if k not in ['created_at', 'updated_at', 'id']:
                     self.__dict__[k] = v
         else:
+
             models.storage.new(self)
 
     def save(self):
@@ -44,6 +60,6 @@ class BaseModel:
         return rdict
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
+        """Return the string representation of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
