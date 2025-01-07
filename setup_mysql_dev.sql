007fb198834b8a7e5b4c0d555b1b41ff602e35df
Thabiso Molefe <thabisomolefe515@gmail.com>
2025-01-06 16:13:51 -0800

    Scripts for setting up MYSQL development and test servers

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,20 @@
+-- Creates a MYSQL server with
+-- Database hbnb_dev_db
+-- User hbnb_dev in localhost witb password hbnb_test_pwd
+-- Grants all priviliges for hbnb_test on hbnb_test_db
+-- Grants SELECT priviliges hbnb_test on performance_schema
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER
+	IF NOT EXISTS 'hbnb_dev'@'localhost'
+	IDENTIFIED  BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES
+	ON `hbnb_dev_db`.*
+	TO 'hnbnb_dev'@'localhost'
+	IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT
+	ON `performance_schema`.*
+
+	TO 'hbnb_dev'@'localhost'
+	IDENTIFIED BY 'hbnb_dev_pwd';
+FLUSH PRIVILEGES
