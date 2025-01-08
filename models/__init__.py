c7af23907ec77495e6df15ab47c0d0f517c96786
Thabiso Molefe <thabisomolefe515@gmail.com>
2025-01-08 06:16:16 -0800

    Moved import statements to the top just after shebang

--- models/__init__.py
+++ models/__init__.py
@@ -6,12 +6,11 @@
 -> Otherwise, instantiates a file storage engine (FileStorage).
 """
 from os import getenv
-
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
