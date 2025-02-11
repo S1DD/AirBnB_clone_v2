fb15768818d92dae534d46440f399d9fd01d1872
Thabiso <thabisomolefe515@gmail.com>
2025-02-11 07:08:46 -0800

    Added indentation to improve readability

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,12 +7,15 @@
           /: Displays 'Hello HBNB!'
 """
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays Hello HNBNB!"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
